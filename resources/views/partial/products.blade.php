cf8c6b0c788df4d107147791b43c738c64d95773
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-10-17 15:49:50 +0800

    added product list

--- resources/views/partial/products.blade.php
+++ resources/views/partial/products.blade.php
@@ -1,3 +1,62 @@
-<div>
-    <p>PRODUCTS CONTENT</p>
+<div class="container-fluid">
+    <div class="row-fluid" style="text-align: center; margin-top: 30px;">
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium placerat ipsum, quis ultrices eros egestas eget. Fusce dolor lectus, euismod id sollicitudin ac, dapibus</p>
+    </div>
+</div>
+
+<div class="row">
+
+    <div class="col" style="padding: 60px;">
+
+        <div class="container">
+            <h2><strong>HOTEL AND PENSION HOUSE</strong></h2>
+        </div>
+        <div class="container">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium placerat ipsum,
+                quis ultrices eros egestas eget. Fusce dolor lectus, euismod id sollicitudin ac, dapibus
+                quis ultrices eros egestas eget. Fusce dolor lectus, euismod id sollicitudin ac, dapibus</p>
+        </div>
+
+        <div class="container">
+            <h2><strong>SCHOOLS</strong></h2>
+        </div>
+        <div class="container">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium placerat ipsum,
+                quis ultrices eros egestas eget. Fusce dolor lectus, euismod id sollicitudin ac, dapibus
+                quis ultrices eros egestas eget. Fusce dolor lectus, euismod id sollicitudin ac, dapibus</p>
+        </div>
+
+    </div>
+
+    <div class="col" style="padding: 60px;">
+
+        <div class="container">
+            <h2><STRONG>RESTAURANTS</STRONG></h2>
+        </div>
+        <div class="container">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium placerat ipsum,
+                quis ultrices eros egestas eget. Fusce dolor lectus, euismod id sollicitudin ac, dapibus
+                quis ultrices eros egestas eget. Fusce dolor lectus, euismod id sollicitudin ac, dapibus</p>
+        </div>
+
+        <div class="container">
+            <h2><STRONG>HARDWARE INVENTORY</STRONG></h2>
+        </div>
+        <div class="container">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium placerat ipsum,
+                quis ultrices eros egestas eget. Fusce dolor lectus, euismod id sollicitudin ac, dapibus
+                quis ultrices eros egestas eget. Fusce dolor lectus, euismod id sollicitudin ac, dapibus</p>
+        </div>
+
+        <div class="container">
+            <h2><STRONG>AUTOSHOP INVENTORY</STRONG></h2>
+        </div>
+        <div class="container">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium placerat ipsum,
+                quis ultrices eros egestas eget. Fusce dolor lectus, euismod id sollicitudin ac, dapibus
+                quis ultrices eros egestas eget. Fusce dolor lectus, euismod id sollicitudin ac, dapibus</p>
+        </div>
+
+    </div>
+
 </div>
