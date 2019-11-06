b6d1080ce4cc499d16542c3b34b5606b03af23ac
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-11-06 22:53:38 +0800

    fixed hyperlinks

--- resources/views/partial/products.blade.php
+++ resources/views/partial/products.blade.php
@@ -9,7 +9,9 @@
     <div class="col" style="padding: 60px;">
 
         <div class="container">
+            <a href="{{route('hotel')}}" style="text-decoration: none; color: dimgray">
             <h2><strong>HOTEL AND PENSION HOUSE</strong></h2>
+            </a>
         </div>
         <div class="container">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium placerat ipsum,
@@ -18,7 +20,9 @@
         </div>
 
         <div class="container">
+            <a href="{{route('schools')}}" style="text-decoration: none; color: dimgray">
             <h2><strong>SCHOOLS</strong></h2>
+            </a>
         </div>
         <div class="container">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium placerat ipsum,
@@ -31,7 +35,9 @@
     <div class="col" style="padding: 60px;">
 
         <div class="container">
+            <a href="{{route('restaurants')}}" style="text-decoration: none; color: dimgray">
             <h2><STRONG>RESTAURANTS</STRONG></h2>
+            </a>
         </div>
         <div class="container">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium placerat ipsum,
@@ -40,7 +46,9 @@
         </div>
 
         <div class="container">
+            <a href="#" style="text-decoration: none; color: dimgray">
             <h2><STRONG>HARDWARE INVENTORY</STRONG></h2>
+            </a>
         </div>
         <div class="container">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium placerat ipsum,
@@ -49,7 +57,9 @@
         </div>
 
         <div class="container">
+            <a href="#" style="text-decoration: none; color: dimgray">
             <h2><STRONG>AUTOSHOP INVENTORY</STRONG></h2>
+            </a>
         </div>
         <div class="container">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium placerat ipsum,
