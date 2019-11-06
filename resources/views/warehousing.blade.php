49443bb8821cbb925f1d9215ef8b71cc03d108ac
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-11-06 22:50:15 +0800

    warehousing

---
+++ resources/views/warehousing.blade.php
@@ -0,0 +1,23 @@
+@extends('layout')
+
+@section('content')
+
+<div class="container-fluid" style="margin-top: 30px;">
+    <div class="row-fluid">
+        <h2 style="text-align: center"><strong>ABOUT WAREHOUSING</strong></h2>
+        <h3 style="text-align: center">Hotel and Pension House</h3>
+
+        <div class="col">
+            <div class="container">
+        <p style="text-align: justify"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium placerat ipsum, quis ultrices eros egestas eget.
+            Fusce dolor lectus, euismod id sollicitudin ac, dapibus @appendLorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Etiam pretium placerat ipsum, quis ultrices eros egestas eget. Fusce dolor lectus, euismod id sollicitudin ac, dapibus
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium placerat ipsum, quis ultrices eros egestas eget.
+            Fusce dolor lectus, euismod id sollicitudin ac, dapibus</p>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+@endsection
