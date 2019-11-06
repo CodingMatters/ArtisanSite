038846308eac7a8e352739743be698d4fd9b7427
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-11-06 22:47:50 +0800

    about us blade

---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,86 @@
+@extends('layout')
+
+@section('content')
+
+    <div class="container-fluid" style="margin-top: 30px; text-align: justify; padding-right: 50px; padding-left: 50px">
+        <div style="color: limegreen;"><h4><strong>HISTORY</strong></h4></div>
+        <div>
+            <p> Coding Matters Software Technology Services (as Coding Matters) is an IT company startup started operating
+                on February 2018. It was built on the idea that it could do more in advancing the technology in Iligan City
+                community through software development. We work everyday to create, customize and design software that would
+                cater to the different needs of our Clients, with the aim of greatly contributing efficiency, betterment and
+                change to the lives of the people in the community.
+            </p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col" style="margin-top: 30px;">
+
+            <div class="card" style="margin-right: 20px; margin-left: 50px">
+                <div class="card-header" style="color: limegreen; text-align: center;">
+                    <strong> - OUR VISION - </strong>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Praesent faucibus nibh eget odio condimentum varius. Aliquam erat volutpat. Sed
+                        lacus orci, tempus eu tellus eget, efficitur ultricies magna. Vivamus pulvinar rhoncus nisi vel
+                        fermentum.</p>
+                </div>
+            </div>
+
+            <div class="card" style="margin-top: 30px; margin-right: 20px; margin-left: 50px">
+                <div class="card-header" style="color: limegreen; text-align: center;">
+                    <strong> - OUR MISSION - </strong>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Praesent faucibus nibh eget odio condimentum varius. Aliquam erat volutpat. Sed
+                        lacus orci, tempus eu tellus eget, efficitur ultricies magna. Vivamus pulvinar rhoncus nisi vel
+                        fermentum.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col" style="margin-top: 30px; margin-bottom: 30px;">
+
+            <div class="card" style="margin-right: 50px; margin-left: 20px">
+                <div class="card-header" style="color: limegreen; text-align: center;">
+                    <strong> - OUR COMPANY VALUES - </strong>
+                </div>
+                <div class="card-body">
+                    <p class="card-text" style="text-align: center;"><strong>One of our goals is to serve our Clients by
+                            living our core values:</strong></p>
+
+                    <p style="color: limegreen"><strong>Commitment</strong></p>
+                    <p style="margin-top: 1px; margin-left: 20px; text-align: justify">We make commitments with care and
+                        live up to them.</p>
+
+                    <p style="color: limegreen"><strong>Teamwork</strong></p>
+                    <p style="margin-top: 1px; margin-left: 20px; text-align: justify">We consider ourselves a team and
+                        work as one. Respect, cooperation and hard work from everyone creates the best environment not
+                        just for the Company  but for the Clients as well.</p>
+
+                    <p style="color: limegreen"><strong>Integrity</strong></p>
+                    <p style="margin-top: 1px; margin-left: 20px; text-align: justify">We uphold honesty and integrity
+                        in everything we do. We maintain high standards of personal integrity, confidentiality and
+                        ethical behavior.</p>
+
+                    <p style="color: limegreen"><strong>Passion</strong></p>
+                    <p style="margin-top: 1px; margin-left: 20px; text-align: justify">We put a heart in what we do and
+                        continuously strive for excellence, with courage to go beyond our comfort zones.</p>
+
+                    <p style="color: limegreen"><strong>Assertiveness</strong></p>
+                    <p style="margin-top: 1px; margin-left: 20px; text-align: justify">We are bold in reaching our goals
+                        amidst pressure. We are not afraid to do the right thing and be the first to do it.</p>
+
+                    <p style="color: limegreen"><strong>Delivering Quality</strong></p>
+                    <p style="margin-top: 1px; margin-left: 20px; text-align: justify">We care about one thing above all,
+                        uilding value for the Company and our Clients. Fast is better than slow but we don’t compromise
+                        quality. We don’t just get the job done, we do it RIGHT.</p>
+
+                </div>
+            </div>
+
+    </div>
+
+
+@endsection
