759d382c70fa0594affd97ba880f738efac4ca75
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-11-06 22:51:51 +0800

    added products and services routes

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,11 @@
 <?php
 
-Route::view("/", "index");
+Route::view("/", "index")->name("home");
+Route::view("/about-us", "about-us")->name("about-us");
+Route::view("/warehousing","warehousing")->name("warehousing");
+Route::view("/payroll","payroll")->name("payroll");
+Route::view("/purchase","purchase")->name("purchase");
+Route::view("/sales","sales")->name("sales");
+Route::view("/hotel","hotel")->name("hotel");
+Route::view("/restaurants","restaurants")->name("restaurants");
+Route::view("/schools","schools")->name("schools");
