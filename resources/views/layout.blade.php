8268d5e838fb4bfea7ff21c17237d074927944c0
IrishCielo <irishcielo.libre@g.msuiit.edu.ph>
2019-11-06 22:55:08 +0800

    added bootstrap source code

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -17,7 +17,7 @@
 
 
         .navbar a:hover {
-            background-color: white;
+            background-color: #f8f9fa;
         }
 
 
@@ -39,7 +39,7 @@
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#"><img class="image-responsive" src="/images/logo.png" style="width: 130px;"></a>
+    <a class="navbar-brand" href="{{route('home')}}"><img class="image-responsive" src="/images/logo.png" style="width: 130px;"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -47,7 +47,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link" href="#"><strong style="color: limegreen">ABOUT US</strong> <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="{{route('about-us')}}"><strong style="color: limegreen">ABOUT US</strong> <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#"><strong style="color: limegreen">CONTACT US</strong></a>
